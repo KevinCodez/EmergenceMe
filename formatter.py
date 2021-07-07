046ab851207c950c4a7e692879d8424e834c5713
Kevin Reynolds <39411500+KevinCodez@users.noreply.github.com>
2021-07-06 20:35:13 -0500

    Standardizes the time format
    - Switches the time format to use the standard 12-hour clock
    - Removes the seconds 
    - e.g. "05:12:45" --> "5:12 AM"

--- formatter.py
+++ formatter.py
@@ -8,7 +8,9 @@ def format_incident(incident):
     # 05-08-2021 | 04:12:00
     incident_time = datetime.datetime.strptime(incident[0], '%m-%d-%Y | %H:%M:%S')
     date = incident_time.strftime("%m/%d/%Y")
-    time = incident_time.strftime("%H:%M:%S")
+    time = incident_time.strftime("%I:%M %p")
+    if time[0] == '0':
+        time = time[1:]
     description = incident[1]
     location = incident[2]
     formatted_incident = [date, time, description, location]
