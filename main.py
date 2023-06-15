3168d805a8533a50caadff47e004053e4b2e306f
Kevin Reynolds <kebbyr99@yahoo.com>
2023-06-15 00:38:25 -0500

    Enable dispatch scraping

--- main.py
+++ main.py
@@ -145,11 +145,11 @@ def sendTextMessage(message, cellNumber):
     time.sleep(3)
 
 while True:
-    # check_for_new_dispatches()
+    check_for_new_dispatches()
     check_for_new_arrests()
     print("Starting 30 minute timer", flush=True)
     time.sleep(1800)
 
-    # check_for_new_dispatches()
+    check_for_new_dispatches()
     print("Starting 30 minute timer", flush=True)
     time.sleep(1800)
